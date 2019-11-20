bea8e861ee01a682c12fa77ef7fd6413c23db16b
Harper <harperliulove@gmail.com>
2019-11-20 21:34:03 +1100

    adding display detail album function

--- assets/main.js
+++ assets/main.js
@@ -302,3 +302,25 @@ function alertMsg(){
     console.log("error");
 }
 
+function displayDetailAlbum(response){
+    var album = response.album[0];
+    // var result = response.artists[0];
+    var card = $("<div>").addClass("col-12 card mb-2");
+    var cardBody = $("<div>").addClass("card-body");
+    var artist = $("<h5>").addClass("card-title").text(album.strAlbum);
+    var year = $("<p>").addClass("card-text").text(album.intYearReleased);
+    // var bio = $("<p>").addClass("card-text").text(result.strBiographyEN);
+    var bio = $("<div>").addClass("card-text");
+    var bioLine = album.strDescriptionEN.split("\n");
+    bioLine.forEach(function(el){
+        var bioEl = $("<p>").text(el);
+        bio.append(bioEl);
+    });    
+    cardBody.append(artist,year,bio);
+    card.append(cardBody);
+    $("#detailsDisplay").append(card);  
+}
+
+function displayDetailLyrics(response){
+    
+}
